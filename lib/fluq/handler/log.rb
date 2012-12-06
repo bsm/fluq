8573c2c442e1a0be60ab01dc0e0d8fdb8cd27c7f
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2012-12-06 17:26:52 +0000

    Added GZIP output to log handler

--- lib/fluq/handler/log.rb
+++ lib/fluq/handler/log.rb
@@ -1,9 +1,12 @@
+require 'zlib'
+
 class FluQ::Handler::Log < FluQ::Handler::Base
 
   # @see FluQ::Handler::Base#initialize
   def initialize(*)
     super
     @full_path = FluQ.root.join(config[:path]).to_s.freeze
+    @gzip      = !!(@full_path =~ /\.gz$/)
     @rewrite   = config[:rewrite]
     @convert   = config[:convert]
   end
@@ -15,16 +18,23 @@ class FluQ::Handler::Log < FluQ::Handler::Base
 
     FileUtils.mkdir_p(File.dirname(path))
     File.open(path, "a+") do |file|
-      file.puts @convert.call(event)
+      write(file, event)
     end
   end
 
   protected
 
+    def write(io, event)
+      io = Zlib::GzipWriter.new(io) if @gzip
+      io.puts(@convert.call(event))
+    ensure
+      io.close
+    end
+
     # Configuration defaults
     def defaults
       super.merge \
-        path: "log/raw/%t/%Y%m%d/%H.log",
+        path: "log/raw/%t/%Y%m%d/%H.log.gz",
         rewrite: lambda {|tag| tag.gsub(".", "/") },
         convert: lambda {|event| event.to_s }
     end
--- spec/fluq/handler/log_spec.rb
+++ spec/fluq/handler/log_spec.rb
@@ -8,27 +8,38 @@ describe FluQ::Handler::Log do
   let(:root)    { FluQ.root.join("../scenario/log/raw") }
   before        { FileUtils.rm_rf(root); FileUtils.mkdir_p(root) }
 
+  def read_gz(file)
+    content = nil
+    Zlib::GzipReader.open(file) {|gz| content = gz.read }
+    content
+  end
 
   it { should be_a(FluQ::Handler::Base) }
   its(:config) { subject.keys.should =~ [:convert, :path, :pattern, :rewrite] }
 
   it 'should log events' do
     subject.on_event(event)
-    file = root.join("my/special/tag/20110812/06.log")
+    file = root.join("my/special/tag/20110812/06.log.gz")
     file.should be_file
-    file.read.should == %(my.special.tag\t1313131313\t{"a":"1"}\n)
+    read_gz(file).should == %(my.special.tag\t1313131313\t{"a":"1"}\n)
+  end
+
+  it "can log plain text" do
+    plain = described_class.new(path: "log/raw/%t/%Y%m%d/%H.log")
+    plain.on_event(event)
+    root.join("my/special/tag/20110812/06.log").read.should == %(my.special.tag\t1313131313\t{"a":"1"}\n)
   end
 
   it 'can have custom conversions' do
     subject = described_class.new convert: lambda {|e| e.merge(ts: e.timestamp).map {|k,v| "#{k}=#{v}" }.join(',') }
     subject.on_event(event)
-    root.join("my/special/tag/20110812/06.log").read.should == "a=1,ts=1313131313\n"
+    read_gz(root.join("my/special/tag/20110812/06.log.gz")).should == "a=1,ts=1313131313\n"
   end
 
   it 'can rewrite tags' do
     subject = described_class.new rewrite: lambda {|t| t.split('.').reverse.first(2).join(".") }
     subject.on_event(event)
-    root.join("tag.special/20110812/06.log").should be_file
+    root.join("tag.special/20110812/06.log.gz").should be_file
   end
 
 end
