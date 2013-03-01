5b2b01e688aea389ea563b64e1975c100a7bbc14
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2013-03-01 18:16:36 +0000

    Bring back optimisation
    This reverts commit 49d3f711050dc4ffebedb98907a811b294fa2fb7.

--- lib/fluq/input/socket/connection.rb
+++ lib/fluq/input/socket/connection.rb
@@ -1,21 +1,39 @@
 class FluQ::Input::Socket::Connection < EventMachine::Connection
   include FluQ::Mixins::Loggable
 
+  attr_reader :queue
+
   # Constructor
   # @param [FluQ::Reactor] reactor
   def initialize(reactor)
     super()
     @reactor = reactor
     @pac     = FluQ::Event::Unpacker.new
+    @queue   = Queue.new
+    reschedule!
   end
 
   # Callback
   def receive_data(data)
-    @pac.feed_slice(data, 10_000) do |events|
-      @reactor.process(events)
+    reschedule!
+    @pac.feed_each(data) do |event|
+      @queue << event
     end
   rescue => ex
     logger.crash "#{self.class.name} failure: #{ex.message} (#{ex.class.name})", ex
   end
+
+  private
+
+    def flush!
+      events = []
+      loop { events << @queue.pop(true) } rescue nil
+      @reactor.process(events) unless events.empty?
+    end
+
+    def reschedule!
+      @timer.cancel if @timer
+      @timer = EM.add_periodic_timer(0.1) { flush! }
+    end
 
 end
