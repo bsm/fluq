49d3f711050dc4ffebedb98907a811b294fa2fb7
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2013-03-01 14:31:44 +0000

    Revert optimisation
    This reverts commit 6136a6d86a4f3d557a4592f18c795ac43c33be61.

--- spec/fluq/input/socket/connection_spec.rb
+++ spec/fluq/input/socket/connection_spec.rb
@@ -4,34 +4,20 @@ describe FluQ::Input::Socket::Connection do
 
   let(:event)    { FluQ::Event.new("some.tag", 1313131313, {}) }
   let!(:handler) { reactor.register FluQ::Handler::Test }
-  let(:timer)    { mock("Timer", cancel: true) }
   subject        { described_class.new(Time.now.to_i, reactor) }
-  before         { EM.stub add_periodic_timer: timer }
 
   it { should be_a(EM::Connection) }
-  its(:queue) { should be_instance_of(Queue) }
+
+  it 'should handle data' do
+    subject.receive_data [event, event].map(&:encode).join
+    FluQ::Testing.wait_until { handler.events.size > 1 }
+    handler.should have(2).events
+  end
 
   it 'should recover connection errors' do
-    subject.queue.should_receive(:<<).and_raise(RuntimeError)
+    reactor.should_receive(:process).and_raise(Errno::ECONNRESET)
     FluQ.logger.should_receive(:crash)
     subject.receive_data [event, event].map(&:encode).join
-  end
-
-  it 'should queue incoming data' do
-    subject.receive_data [event, event].map(&:encode).join
-    subject.receive_data [event, event, event].map(&:encode).join
-    subject.queue.should have(5).items
-  end
-
-  it 'should flush queue when idle' do
-    subject.receive_data [event, event].map(&:encode).join
-    subject.receive_data [event, event, event].map(&:encode).join
-    subject.receive_data [event].map(&:encode).join
-    lambda {
-      subject.send(:flush!)
-    }.should change { subject.queue.size }.from(6).to(0)
-    FluQ::Testing.wait_until { handler.events.size > 5 }
-    handler.should have(6).events
   end
 
 end
