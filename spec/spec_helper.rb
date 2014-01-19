f0ce4c167d7043a4a0ddb380afd1bb89101047a0
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2014-01-19 09:15:50 +0000

    Include Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ ENV['FLUQ_ROOT'] ||= File.expand_path("../scenario/", __FILE__)
 require 'bundler/setup'
 require 'rspec'
 require 'coveralls'
+Coveralls.wear_merged!
 
 require 'fluq/testing'
 FluQ.logger = Logger.new(FluQ.root.join("log", "fluq.log").to_s)
